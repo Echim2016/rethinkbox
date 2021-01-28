eb00b03c32233609d235f7dd49b6b28c787b957a
Echim2016 <ychxz26@gmail.com>
2021-01-28 16:30:21 +0800

    initialization

---
+++ js/index1.js
@@ -0,0 +1,187 @@
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+		var colors = [
+			'#490A3D',
+			'#BD1550',
+			'#E97F02',
+			'#F8CA00',
+			'#8A9B0F',
+			'#69D2E7',
+			'#FA6900',
+			'#16a085',
+			'#27ae60',
+			'#2c3e50',
+			'#f39c12',
+			'#e74c3c',
+			'#9b59b6',
+			'#FB6964',
+			'#342224',
+			'#472E32',
+			'#77B1A9',
+			'#73A857'
+		];
+
+
+     var topic = ["嗨，想聊些什麼？"];
+     var group = ["這是問題分類"];
+     var group2 = [""];
+     // var estTime = [];
+
+     // var dislike = [""];
+     // var like = [""];
+     var featured =[]
+     var weight = [];
+     var no = [];
+     var like = [];
+     var dislike = [];
+
+     var drawArray=[];
+     var noArray=[];
+     var disArray=[];
+     var likeArray=[];
+     var featuredArray=[];
+
+     var randomquote = 0;
+     var randomcolor = "";
+     var currentQuote = "迴響隨身錦囊報給你知，點擊「說說看」抽出一個錦囊！";
+
+
+     $.getJSON('https://spreadsheets.google.com/feeds/list/1JMQMEp269AJuPhaFJYjvePRlaWWTSD2NyeVx-VG6F7E/od6/public/values?alt=json', function (data){
+
+       for (var i = 0; i < data.feed.entry.length; i++){
+            topic[i] = data.feed.entry[i].gsx$topic['$t'];
+            // group[i] = data.feed.entry[i].gsx$group['$t'];
+            // group2[i] = data.feed.entry[i].gsx$group2['$t'];
+            // estTime[i]= data.feed.entry[i].gsx$time['$t'];
+            featured[i]= data.feed.entry[i].gsx$featured['$t'];
+            like[i]= data.feed.entry[i].gsx$like['$t'];
+            dislike[i]= data.feed.entry[i].gsx$dislike['$t'];
+            weight[i]= data.feed.entry[i].gsx$weight['$t'];
+            no[i]= data.feed.entry[i].gsx$no['$t'];
+
+            for (var j = 0; j < weight[i]; j++) {
+              drawArray.push(topic[i]);
+              noArray.push(no[i]);
+              disArray.push(dislike[i]);
+              likeArray.push(like[i]);
+              featuredArray.push(featured[i]);
+
+            }
+      }
+      // getQuote();
+      });
+
+
+
+function pressDislike(){
+  var url='https://script.google.com/macros/s/AKfycbyka_VfjwoIsVAInzmto_jSxf-UClo5-gjbHuiNgn6caG48Qys/exec';
+  // console.log(disArray[randomquote]);
+  // console.log(randomquote);
+  // console.log(noArray[randomquote]);
+  $.ajax({
+        url: url,
+        type: 'GET',
+        data:{
+          "like":0,
+          "dislike":disArray[randomquote],
+          "row":noArray[randomquote],
+          "column":9,
+          "action":"dislike"
+        },
+        success: function(res) {
+            // alert("Successfully submitted");
+        }
+    });
+    getQuote();
+}
+
+function pressLike(){
+  var url='https://script.google.com/macros/s/AKfycbyka_VfjwoIsVAInzmto_jSxf-UClo5-gjbHuiNgn6caG48Qys/exec';
+  $.ajax({
+        url: url,
+        type: 'GET',
+        data:{
+          "like":likeArray[randomquote],
+          "dislike":0,
+          "row":noArray[randomquote],
+          "column":8,
+          "action":"like"
+        },
+        success: function(res) {
+
+           //  Swal.fire({
+           //    type: 'success',
+           //    title: '讚啦，祝你有個愉快的Check-in時光！',
+           //    showConfirmButton: false,
+           //    timer: 6000
+           //  })
+           //  setTimeout(function(){// wait for 5 secs(2)
+           //      location.reload(); // then reload the page.(3)
+           // }, 2000);
+        }
+    });
+    getQuote();
+
+
+}
+
+
+var currentText = "";
+var getQuoteCount =0;
+
+function getQuote() {
+    getQuoteCount++;
+    currentText ="";
+    randomquote = Math.floor(Math.random() * drawArray.length);
+
+      currentQuote = drawArray[randomquote];
+      currentText = featuredArray[randomquote];
+
+    // }
+
+	$(document).ready(function () {
+	    $('html body').animate({
+	        backgroundColor: colors[randomcolor],
+	        color: colors[randomcolor]
+	    }, 500);
+	    $('#newquote, .social-icons .fa-twitter').animate({ backgroundColor: colors[randomcolor] }, 500);
+			$('blockquote footer').animate({ color: colors[randomcolor] }, 500);
+	    $('blockquote').animate({ borderLeftColor: colors[randomcolor] }, 500);
+	    $('#quotetext').animate({ opacity: 0 }, 500, function () {
+	        $(this).animate({ opacity: 1 }, 500);
+	        $(this).text(currentQuote);
+	    });
+	    $('#quotesource').animate({ opacity: 0 }, 300, function () {
+	        $(this).animate({ opacity: 0.7 }, 500);
+	        $(this).text(currentText);
+	    });
+    });
+
+}
+
+function openURL(url) {
+    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+
+$(document).ready(function () {
+    setTimeout(function(){
+      $('html,body').animate({scrollTop: 200},600);
+    },1500)
+    $('#quotetext').animate({ opacity: 0 }, 800, function () {
+        $(this).animate({ opacity: 0.5 }, 500);
+        $(this).text(currentQuote);
+    });
+    $('#newquote').on('click', getQuote);
+    $('#btnDislike').on('click', pressDislike);
+    $('#btnLike').on('click', pressLike);
+    $('#tweetquote').on('click', function () {
+        if (!inIframe()) {
+            openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+        }
+    });
+});
---
+++ js/input.js
@@ -0,0 +1,50 @@
+
+var $form = $('#contactForm'),
+    url = 'https://script.google.com/macros/s/AKfycbyka_VfjwoIsVAInzmto_jSxf-UClo5-gjbHuiNgn6caG48Qys/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serialize(),
+    success: function(response){
+      Swal.fire({
+        type: 'success',
+        title: 'weeee！！已收到你的回應囉！',
+        background: '#DCDCDC',
+        showConfirmButton: false,
+        timer: 1800
+      })
+      setTimeout(function(){// wait for 5 secs(2)
+          // location.reload(); // then reload the page.(3)
+          window.location.href="index.html";
+     }, 2100);
+      return true;
+    }
+  })
+})
+
+var resetClick =0;
+
+$('#reset-click').on('click',function(){
+
+  resetClick++;
+
+  if(resetClick==7){
+
+    Swal.fire({
+      type: 'success',
+      title: '很會點喔',
+      background: '#DCDCDC',
+      showConfirmButton: false,
+      timer: 1800
+    })
+
+    resetClick =0;
+  }
+
+
+})
